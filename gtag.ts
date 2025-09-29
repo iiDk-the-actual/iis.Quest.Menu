a254b57b040b2919bee4606c47737c960231bf45
iiDk (Grayson G.) <54154105+iiDk-the-actual@users.noreply.github.com>
2025-09-28 22:57:05 -0400

    Update gtag.ts

--- gtag.ts
+++ gtag.ts
@@ -5,7 +5,7 @@ declare const Il2Cpp: any;
 declare const console: any;
 declare const XRNode: any;
 
-const version = "1.3.0";
+const version = "1.4.0";
 let boardMaterial = null;
 let buttonClickDelay = 0.0;
 let menu = null;
